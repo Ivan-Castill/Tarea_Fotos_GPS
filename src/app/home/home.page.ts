fa1e1de3e514acf655b1b7a022d7f2f0ee0571cf
Ivan-Castill <ivan2003castillo12345@gmail.com>
2025-10-21 00:20:26 -0500

    Version 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { PhotoService } from '../services/photo';
+import { LocationService } from '../services/location';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,59 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit , OnDestroy {
+  latitude = signal<number | null>(null);
+  longitude = signal<number | null>(null);
+  watchId: string | null = null;
+  errorMsg = signal<string | null>(null);
 
-  constructor() {}
+  constructor(public photoService: PhotoService, private loc: LocationService) {}
 
+  async ngOnInit() {
+    // Cargar las fotos guardadas al iniciar la pestaña
+    await this.photoService.loadSaved();
+    await this.loc.ensurePermissions();
+    await this.obtenerUbicacionActual();
+    await this.iniciarSeguimiento();
+  }
+
+  async obtenerUbicacionActual() {
+    try {
+      const pos = await this.loc.getCurrentPosition();
+      this.latitude.set(pos.coords.latitude);
+      this.longitude.set(pos.coords.longitude);
+      this.errorMsg.set(null);
+    } catch (e: any) {
+      this.errorMsg.set(e?.message ?? 'Error al obtener la ubicación actual');
+    }
+  }
+
+  async iniciarSeguimiento() {
+    try {
+      this.watchId = await this.loc.watchPosition((pos) => {
+        this.latitude.set(pos.coords.latitude);
+        this.longitude.set(pos.coords.longitude);
+      }, (err) => {
+        this.errorMsg.set(err?.message ?? 'Error en seguimiento de ubicación');
+      });
+    } catch (e: any) {
+      this.errorMsg.set(e?.message ?? 'No se pudo iniciar el seguimiento');
+    }
+  }
+
+  async detenerSeguimiento() {
+    if (this.watchId) {
+      await this.loc.clearWatch(this.watchId);
+      this.watchId = null;
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.watchId) this.loc.clearWatch(this.watchId);
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+    console.log('Foto añadida a la galería');
+  }
 }
